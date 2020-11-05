7896ec0a05f84f2f74de3d3f8df93a4b39b81d82
tacheometry <39647014+tacheometry@users.noreply.github.com>
2020-11-06 00:47:19 +0200

    JSDoc documentation. v2 basically done

--- SimpleSignals.ts
+++ SimpleSignals.ts
@@ -83,49 +83,66 @@ const BindableManager = new class {
 
 abstract class SimpleShared {
 	/**
-	 * Connect to a RemoteEvent via `callback`
+	 * Connect to `name`'s corresponding RemoteEvent via `callback`. On the server `OnServerEvent` gets connected. On the client, `OnClientEvent`.
+	 * @param name The name of the RemoteEvent to connect to.
+	 * @param callback The callback function.
+	 * @returns The RBXScriptConnection returned when connecting to this RemoteEvent's RBXScriptSignal.
 	 */
 	abstract on(name: string, callback: Callback): RBXScriptConnection;
 	
 	/**
-	 * Connect to a RemoteEvent **only once** via `callback` 
+	 * Connect to a RemoteEvent **only once** via `callback`. Functions the same as `simple.on`. This implementation doesn't use any Wait methods.
+	 * @param name The name of the RemoteEvent to connect once to.
+	 * @param callback The callback function.
 	 */
 	 abstract once(name: string, callback: Callback): void;
 	
 	 /**
-	 * Fire a RemoteEvent with the specified `args`
+	 * Fire a RemoteEvent with the specified arguments. On the server `FireServer` gets run. On the client, `FireClient`.
+	 * @param name The name of the RemoteEvent to fire.
+	 * @param args The arguments to fire the RemoteEvent with.
 	 */
 	abstract fire(name: string, ...args: unknown[]): void;
 	
 	/**
-	 * Invoke a RemoteFunction with the specified `args`
+	 * Invoke a RemoteFunction with the specified arguments. On the server `InvokeClient` gets run. On the client, `InvokeServer`. Be careful when invoking to a client!
+	 * @param name The RemoteFunction to invoke.
+	 * @param args The arguments to pass to the invoke function.
 	 * @yields
 	 */
 	abstract get(name: string, ...args: unknown[]): unknown;
 	
 	/**
-	 * Set the RemoteFunction's OnServerInvoke/OnClientInvoke callback to `callback`
+	 * Set the RemoteFunction's `On ... Invoke` callback. On the server, `OnServerInvoke` gets set. On the client, `OnClientInvoke`.
+	 * @param name The RemoteFunction to set the callback for.
+	 * @param callback The callback function.
 	 */
 	abstract setCallback(name: string, callback: Callback): void;
 
 	/**
-	 * Fire a BindableEvent with the specified `args`
+	 * Fire a BindableEvent with the specified arguments.
+	 * @param name The BindableEvent to fire.
+	 * @param args The arguments to pass to the fire function.
 	 */
 	abstract fireBindable(name: string, ...args: unknown[]): void;
 
 	/**
-	 * Connect to a BindableEvent via `callback`
+	 * Connect to `name`'s corresponding BindableEvent via `callback`.
+	 * @param name The name of the BindableEvent to connect to.
+	 * @param callback The callback function.
 	 */
 	abstract onBindable(name: string, callback: Callback): RBXScriptConnection;
 
 	/** 
-	 * Connect to a BindableEvent **only once** via `callback`
+	 * Connect to a BindableEvent **only once** via `callback`. Functions the same as `simple.onBindable`. This implementation doesn't use any Wait methods.
+	 * @param name The name of the BindableEvent to connect to.
+	 * @param callback The callback function.
 	*/
 	abstract onceBindable(name: string, callback: Callback): void;
 }
 
-export const SimpleServer = new class implements SimpleShared {
-	on(name: string, callback: Callback): RBXScriptConnection {
+class SimpleServer implements SimpleShared {
+	on(name: string, callback: (player: Player, ...args: any[]) => any): RBXScriptConnection {
 		const event = RemoteManager.GetEvent(name);
 
 		return event.OnServerEvent.Connect(callback);
@@ -142,7 +159,7 @@ export const SimpleServer = new class implements SimpleShared {
 			});
 
 			connection.Disconnect();
-		})
+		});
 	}
 
 	fire(name: string, player: Player, ...args: unknown[]): void {
@@ -196,7 +213,7 @@ export const SimpleServer = new class implements SimpleShared {
 	}
 }
 
-export const SimpleClient = new class implements SimpleShared {
+class SimpleClient implements SimpleShared {
 	on(name: string, callback: Callback): RBXScriptConnection {
 		const event = RemoteManager.GetEvent(name);
 
@@ -261,3 +278,6 @@ export const SimpleClient = new class implements SimpleShared {
 		});
 	}
 }
+
+export const Server = new SimpleServer();
+export const Client = new SimpleClient();
